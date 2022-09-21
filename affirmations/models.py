88899c00d24e07b20082720c7f674971d717338d
mstanle54 <mstanle54@gmail.com>
2022-09-21 11:08:37 -0600

    had to create new  branch after updating ubuntu

---
+++ affirmations/models.py
@@ -0,0 +1,23 @@
+import datetime
+from django.db import models
+from django.contrib.auth.models import User
+
+#
+#from django.utils import timezone
+#
+
+# Create your models here.
+# class Affirmations(models.Model):
+    #affirmationText = models.Charfield(max_length=100)
+    #categoryID = models.IntegerField()
+    #categoryName = models.Charfield(max_length=30)
+    #categoryName = models.ForeignKey()
+    #createdDate = models.DateField(default=tdatetime.datetime.now)
+    #slug = models.SlugField(default="")
+
+    #useful if wanting to print objects in the shell
+    #def _str_(self):
+
+    #class Meta:
+    #Ascending by date published
+    #ordering = ['createdDate']
