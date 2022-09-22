38e2dc2656ca86182d1c112425680ff9aa70cd83
mstanle54 <mstanle54@gmail.com>
2022-09-21 20:12:08 -0600

    creating home page layout

--- affirmations/views.py
+++ affirmations/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'hello.html')
+    return render(request, 'home.html')
 
 def affirmations(request):
     return render(request, 'affirmationsBox.html')    
